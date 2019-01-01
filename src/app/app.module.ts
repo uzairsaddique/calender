2ca68f46581f72f8aac4e0f95f7e39e7ea254faa
uzairsaddique <uzairsaddique@gmail.com>
2019-01-01 15:05:11 +0500

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
+import 'core-js/es6/reflect';
+import 'core-js/es7/reflect';
+import 'zone.js/dist/zone';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { DemoModule } from './demo/demo.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    DemoModule
   ],
   providers: [],
   bootstrap: [AppComponent]
